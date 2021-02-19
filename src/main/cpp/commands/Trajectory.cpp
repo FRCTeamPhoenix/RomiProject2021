766e818f37b0ebe42a09b75eb2acf8ea9eedc86b
Leo <leo.bunkartas@asdnh.org>
2021-02-18 19:12:37 -0500

    Added basic class functions

--- src/main/cpp/commands/Trajectory.cpp
+++ src/main/cpp/commands/Trajectory.cpp
@@ -2,3 +2,24 @@
 
 #include <frc/smartdashboard/SmartDashboard.h>
 #include <iostream>
+
+
+   Trajectory::Trajectory(Drivetrain* drivetrain){
+       AddRequirements({drivetrain});
+   }
+
+    void Trajectory::Initialize(){
+        m_drivetrain->ArcadeDrive(0.0, 0.0);
+    }
+
+    void Trajectory::Execute(){
+       
+    }
+
+    void Trajectory::End(bool interrupted){
+        m_drivetrain->ArcadeDrive(0.0, 0.0);
+    }
+
+    bool Trajectory::IsFinished(){
+            return m_drivetrain->GetAverageDistance() >= m_distanceToGo;
+    }
--- src/main/include/commands/Trajectory.h
+++ src/main/include/commands/Trajectory.h
@@ -1,4 +1,27 @@
 #pragma once
 
 #include "subsystems/Drivetrain.h"
+#include "Constants.h"
 
+#include <frc2/command/CommandBase.h>
+#include <frc2/command/CommandHelper.h>
+#include <frc/controller/PIDController.h>
+
+class Trajectory : public frc2::CommandHelper<frc2::CommandBase, Trajectory>{
+public:
+    Trajectory(Drivetrain* drivetrain);
+
+    void Initialize() override;
+
+    void Execute() override;
+
+    void End(bool interrupted) override;
+
+    bool IsFinished() override;
+
+private:
+    Drivetrain* m_drivetrain;
+    units::meter_t m_distanceToGo;
+    frc2::PIDController m_pid{0.0, 0.0, 0.0};
+
+};
