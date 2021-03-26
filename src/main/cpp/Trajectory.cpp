5f762efa9fda6123e03daeff56073a38fe2d54b4
Robert White <rjwhitenh@outlook.com>
2021-03-25 20:01:35 -0400

    Added some points to a trajectory but robot randomly stopped following them

--- src/main/cpp/Trajectory.cpp
+++ src/main/cpp/Trajectory.cpp
@@ -22,9 +22,15 @@ frc2::RamseteCommand Trajectory::GenerateRamseteCommand(Drivetrain* drive){
     config.SetKinematics(TRAJECTORY::DRIVE_KINEMATICS);
     config.AddConstraint(voltageConstraint);
 
+
     frc::Trajectory firstTrajectory = frc::TrajectoryGenerator::GenerateTrajectory(
       {frc::Pose2d(0_m, 0_m, frc::Rotation2d(0_deg)),
-      frc::Pose2d(1_m, 10_cm, frc::Rotation2d(0_deg))},
+      frc::Pose2d(38_cm, 8_cm, frc::Rotation2d(45_deg)),
+      frc::Pose2d(45_cm, 29_cm, frc::Rotation2d(90_deg))},
+      //frc::Pose2d(35_cm, 44_cm, frc::Rotation2d(180_deg)),
+      //frc::Pose2d(-15_cm, 34_cm, frc::Rotation2d(180_deg)),
+      //frc::Pose2d(-25_cm, 44_cm, frc::Rotation2d(90_deg))},
+      //frc::Pose2d(22_cm, 70_cm, frc::Rotation2d(0_deg))},
       config);
 
     frc2::RamseteCommand ramseteCommand(
